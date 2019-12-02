8a33398eae0dbcb661fb7103650ec91457ea3fec
peng <1826493691@qq.com>
2019-12-02 16:01:15 +0800

    [feat]完成播放条样式修改

--- src/components/musicBar/index.vue
+++ src/components/musicBar/index.vue
@@ -3,26 +3,28 @@
         <div v-if="loading">
             <img src="../../assets/images/loading.png"/>
         </div>
-        <div  v-else>
-            <router-link class="imageContainer" to="/music">
+        <div　v-else>
+            <div class="songDetail">
+                <audio ref="audio" :src="musicUrl" preload="auto" @canplay="getDuration" @timeupdate="upDateTime">您的浏览器不支持audio播放</audio>
+                <router-link class="imageContainer" to="/music">
                 <img :src="musicDetail.imgUrl"/>
-            </router-link>
-            <p class="songName">{{musicDetail.name}} - <span>{{musicDetail.singer}}</span></p>
-            <audio 
-            ref="audio" 
-            :src="musicUrl" 
-            preload="auto"
-            @canplay="getDuration"
-            @timeupdate="upDateTime">您的浏览器不支持audio播放</audio>
-            <span class="timeCounting">{{currentTime}} / {{duration}}</span>
+                </router-link>
+                <p>
+                <span>{{musicDetail.name}}</span>
+                <span> - {{musicDetail.singer}}</span>
+                <span class="timeCounting">{{currentTime}} / {{duration}}</span>
+                </p>
+            </div>
             <div class="controlBtns">
                 <button @click="prev">上一首</button>
                 <button class="mode" @click="togglePlaying"></button>
                 <button @click="next">下一首</button>
-                <button class="volume"></button>
+            </div>
+            <div class="volume">
+                <button></button>
+                <input type="range" max="10" min="0" value="5"/>
             </div>
         </div>
-       
     </div>
 </template>
 <script src="./index.js"></script>
