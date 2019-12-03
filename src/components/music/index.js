47fe8f4fbc4529560f046d26ec4b9b7ad6b83b92
peng <1826493691@qq.com>
2019-12-03 15:52:00 +0800

    [perf]歌曲详情页数据可从vuex中获取，若没有相应数据再进行网络请求

--- src/components/music/index.js
+++ src/components/music/index.js
@@ -20,11 +20,46 @@ export default{
         }
     },
     methods: {
-        getSongMessage(songId){
-        // 获取歌词
-        axios.get('./lyric',{
+        getSongMessage(newId){
+        let musicLists = this.$store.state.musicLists;
+        let songId = null;
+        let song = null;
+        for(let j in musicLists){
+            if(musicLists[j].id === newId){
+                songId = newId;
+                song = musicLists[j];
+            }
+        }
+        // 获取歌曲详情
+        if(songId){
+           this.musicDetail = {
+               name:song.name,
+               album:song.album,
+               singer:song.singer,
+               img:song.img
+           }
+        }else{
+            axios.get('/song/detail',{
+                params:{
+                    ids:newId
+                }
+            }).then((result)=>{
+                let rawMusic = result.data.songs[0];
+                this.musicDetail = { 
+                name:rawMusic.name,
+                album:rawMusic.al.name,
+                singer:rawMusic.ar[0].name,
+                img:rawMusic.al.picUrl
+                };
+            }).catch((err)=>{
+                console.log(err);
+            })
+        }
+
+        //获取歌词 
+        axios.get('/lyric',{
             params:{
-                id:songId
+                id:newId
             }
         }).then((result)=>{
             if(result.data.code !==200){
@@ -33,24 +68,7 @@ export default{
             this.lyric = result.data.lrc.lyric.replace(/\[(.+?)\]/g,'');
         }).catch((err)=>{
             console.log(err);
-        }),
-
-        // 获取歌曲详情
-        axios.get('./song/detail',{
-            params:{
-                ids:songId
-            }
-        }).then((result)=>{
-            let rawMusic = result.data.songs[0];
-            this.musicDetail = { 
-            name:rawMusic.name,
-            album:rawMusic.al.name,
-            singer:rawMusic.ar[0].name,
-            img:rawMusic.al.picUrl
-            };
-        }).catch((err)=>{
-            console.log(err);
-        })
+        }),  
         this.loading = false;
         }
     },
